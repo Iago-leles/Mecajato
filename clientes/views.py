79c52d4631e02b10aa61e2b7bb0401a51cbd7498
Iago-leles <iago.leles@gmail.com>
2022-11-18 16:55:05 -0300

    integrando com banco de dados sqlite

--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente, Carro
 
 def clientes(request):
-    return render(request, 'clientes.html')
+    if request.method =="GET":
+        return render(request, 'clientes.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+
+        cliente.save()
+
+        for carro, placa, ano in zip(carros, placas, anos):
+            car = Carro(carro = carro, placa=placa, ano=ano, cliente=cliente)
+            car.save()
+
+        return HttpResponse('teste')
